a178535f494ef504ece296fb59aa6e72fcc941b2
Joseado381 <josealonzo.jaa@gmail.com>
2025-06-03 16:17:48 -0300

    primer commit del tp

---
+++ ToDo/ToDo.cs
@@ -0,0 +1,10 @@
+namespace TareaToDo;
+
+public class TareaToDo
+{
+    public int TareaId { get; set; }
+    public string Descripcion { get; set; }
+    public int Duracion { get; set; }
+}
+
+List<Tareas> TareasPemnidentes = [];
