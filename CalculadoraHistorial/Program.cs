ef18288035374baab6efdc468699c95ca1d06722
Joseado381 <josealonzo.jaa@gmail.com>
2025-07-02 12:19:05 -0300

    Se me olvido de pushear todos los cambios de los ultimos tps

---
+++ CalculadoraHistorial/Program.cs
@@ -0,0 +1,56 @@
+﻿using Operaciones;
+
+int bandera = 1;
+
+List<Operacion> operaciones = new List<Operacion>();
+int i = 0;
+Console.WriteLine("---Bienvenido a la calculadora:");
+
+while (bandera == 1)
+{
+    Console.WriteLine("Elija la operacion que desea realizar:");
+    Console.WriteLine("1. Mostrar el historial.");
+    Console.WriteLine("2. Operar");
+    string entrada = Console.ReadLine() ?? "";
+    int opcion;
+    int.TryParse(entrada, out opcion);
+    switch (opcion)
+    {
+        case 1:
+            foreach (Operacion item in operaciones)
+            {
+                Console.WriteLine($"La {item.Operaciones} de {item.ResultadoAnterior} y {item.NuevoValor} es {item.Resultado()}");
+            }
+            break;
+        default:
+            Console.WriteLine("Elija la operacion que desea realizar:");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Producto");
+            Console.WriteLine("4. Division");
+
+            entrada = Console.ReadLine() ?? "";
+            TipoOperacion Operaciones;
+            TipoOperacion.TryParse(entrada, out Operaciones);
+
+            double nuevoValor;
+            Console.WriteLine("Ingrese el segundo termino:");
+            entrada = Console.ReadLine() ?? "";;
+            double.TryParse(entrada, out nuevoValor);
+            if (operaciones.Count == 0) //diferencio entre primer componente y el resto porque el resultado anterior, la primera vez debe ser 0
+            {
+                operaciones.Add(new Operacion(0, nuevoValor, Operaciones));
+            }
+            else
+            {
+                double resultadoAnterior = operaciones[i].Resultado(); //i aun no fue incrementado, ni se agrego un  nuebo elemetno a la lista, por lo que es el indice del ultimo elemento
+                operaciones.Add(new Operacion(resultadoAnterior, nuevoValor, Operaciones));
+                i++;
+                Console.WriteLine($"La {operaciones[i].Operaciones} de {operaciones[i].ResultadoAnterior} y {operaciones[i].NuevoValor} es {operaciones[i].Resultado()}");
+            }
+            break;
+    }
+    Console.WriteLine("Desea realizar otra operacion? \n1-Si. \n2-No.");
+    entrada = Console.ReadLine() ?? "";
+    int.TryParse(entrada, out bandera);
+}
--- ToDo/ToDo.cs
+++ ToDo/ToDo.cs
@@ -1,19 +1,45 @@
-namespace TareaToDo
+namespace TareaTodo;
+
+public class Tarea
 {
-    public class TareaToDo
+    public enum EstadoTarea
     {
-        public enum TareaEstado
-        {
-            pendiente,
-            completado
-        }
-
-        public int TareaID {get ; set;}
-        public string Descripcion {get ; set;}
-        public int Duracion {get ; set;}
-
+        Pendiente = 0,
+        Realizada = 1
     }
 
+    private int tareaID;
+    private string? descripcion;
+    private int duracion;
+
+    EstadoTarea estado;
+
+    public int TareaID { get => tareaID; set => tareaID = value; }
+    public string? Descripcion { get => descripcion; set => descripcion = value; }
+    public int Duracion
+    {
+        get => duracion; set => duracion = value;
+    }
+    public EstadoTarea Estado { get => estado; set => estado = value; }
+
+    public Tarea(int tareaID, string descripcion, int duracion, EstadoTarea estado)
+    {
+        this.tareaID = tareaID;
+        this.descripcion = descripcion;
+        this.duracion = duracion;
+        this.estado = estado;
+    }
 }
+
+public class generadorID
+{
+    public static int id = 1000;
+
+    public static int ObtenerID()
+    {
+        return id++;
+    }
+}
+public class Ramdom;
 
 
